1fbf4bc3ac18d5d11c192ca996d6be69c3291a4a
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2021-02-04 19:27:18 +0000

    Created the basic template of the website

---
+++ Website/components/navbar.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+
+<header id="myBanner" role="banner" class="theBanner">
+
+    <h1 id="logo">My Website</h1>
+    <input type="checkbox" id="nav-toggle" class="nav-toggle">
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#projectsBlock">Projects</a></li>
+            <li><a href="#aboutBlock">About</a></li>
+            <li><a href="#languageBlock">Languages</a></li>
+
+        </ul>
+    </nav>
+    <label for="nav-toggle" class="nav-toggle-label">
+        <span></span>
+    </label>
+</header>
+
---
+++ Website/index.php
@@ -0,0 +1,40 @@
+<?php
+include_once "components/navbar.php";
+include_once "components/footer.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en"/>
+<head>
+    <meta charset="UTF-8">
+    <link href="css/mainStyle.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/fca550da2e.js" crossorigin="anonymous"></script>
+    <title>Linked data app</title>
+</head>
+<body>
+<div class="block" id="welcomeBlock">
+    <div class="dark">
+        <h2 class="blockHeader">Welcome</h2>
+    </div>
+</div>
+<div class="block" id="projectsBlock">
+    <div class="lighter">
+        <h2 class="blockHeader">Projects</h2>
+    </div>
+</div>
+<div class="block" id="aboutBlock">
+    <div class="dark">
+        <h2 class="blockHeader">About</h2>
+    </div>
+</div>
+<div class="block" id="languageBlock">
+    <div class="lighter">
+        <h2 class="blockHeader">Languages</h2>
+    </div>
+</div>
+
+</body>
+
+
+
