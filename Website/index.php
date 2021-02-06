89735e00138b7a17c0b7c2294458f712f9578d56
JMChurchill <57601700+JMChurchill@users.noreply.github.com>
2021-02-06 14:55:14 +0000

    Added smooth scroll

--- Website/index.php
+++ Website/index.php
@@ -24,27 +24,29 @@ include_once "components/footer.php";
 
         </div>
 
+<!--        <div class="container">-->
+            <div class="block" id="welcomeBlock">
+                <div class="dark">
+                    <h2 class="blockHeader">Welcome</h2>
+                </div>
+            </div>
+            <div class="block" id="projectsBlock">
+                <div class="lighter">
+                    <h2 class="blockHeader">Projects</h2>
+                </div>
+            </div>
+            <div class="block" id="aboutBlock">
+                <div class="dark">
+                    <h2 class="blockHeader">About</h2>
+                </div>
+            </div>
+            <div class="block" id="languageBlock">
+                <div class="lighter">
+                    <h2 class="blockHeader">Languages</h2>
+                </div>
+            </div>
+<!--        </div>-->
 
-        <div class="block" id="welcomeBlock">
-            <div class="dark">
-                <h2 class="blockHeader">Welcome</h2>
-            </div>
-        </div>
-        <div class="block" id="projectsBlock">
-            <div class="lighter">
-                <h2 class="blockHeader">Projects</h2>
-            </div>
-        </div>
-        <div class="block" id="aboutBlock">
-            <div class="dark">
-                <h2 class="blockHeader">About</h2>
-            </div>
-        </div>
-        <div class="block" id="languageBlock">
-            <div class="lighter">
-                <h2 class="blockHeader">Languages</h2>
-            </div>
-        </div>
 
         <script src="js/mainFunctions.js"></script>
 
